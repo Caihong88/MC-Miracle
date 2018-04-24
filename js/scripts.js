5daf4324a6fe20fb47c9bee07097f45d32d78a49
Sarah <36570316+quadtych@users.noreply.github.com>
2018-04-24 18:58:42 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
+function myFunction() {
+var x = document.getElementById("myName").pattern;
+document.getElementById("demo").innerHTML = x;
+}
 
+function myFunction() {
+var x = document.getElementById("myPhone").pattern;
+document.getElementById("demo").innerHTML = x;
+}
+
+function myFunction() {
+var x = document.getElementById("myEmail").pattern;
+document.getElementById("demo").innerHTML = x;
+}
